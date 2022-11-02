b70081fcb48591e16c8658834a2e28ae81670b9f
AwfulAsiful <awful872@gmail.com>
2022-11-02 22:10:53 +0600

    carlist features implemented

---
+++ source/carlist.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <conio.h>
+
+#include <unistd.h>
+
+using namespace std;
+class carlist
+{
+public:
+    void show_car_list()
+    {
+        cout << "****************************************CAR DETAILS***********************************\n\n";
+        ifstream in("car_list.txt");
+        string str;
+        int cntr = 1;
+        while (getline(in, str))
+        {
+            string str1;
+            cout << "Car number : " << cntr << endl;
+            getline(in, str1);
+            cout << "Car name : " << str << endl;
+            cout << "Car CC   : " << str1 << " CC" << endl
+                 << endl;
+            cntr++;
+        }
+
+        in.close();
+       
+    }
+};
